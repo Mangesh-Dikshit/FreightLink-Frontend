c15dfa55ed8d2a777cb3af5d59871fe53575360a
Lekhya <2462668@cognizant.com>
2025-12-16 14:44:26 +0530

    Create Landing Page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './header/header';
+import { Footer } from './footer/footer';
+import { Landing } from './landing/landing';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Header,Footer,Landing],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
